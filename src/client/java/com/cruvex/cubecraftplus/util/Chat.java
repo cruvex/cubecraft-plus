dfe18c8943af171071c5dc71224e08ad7fe8596f
cruvex <cruvexx@gmail.com>
2026-09-14 16:40:27 +0200

    feat: lobby chest finder

---
+++ src/client/java/com/cruvex/cubecraftplus/util/Chat.java
@@ -0,0 +1,16 @@
+package com.cruvex.cubecraftplus.util;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+
+public class Chat {
+
+    public static void send(Component message) {
+        Minecraft client = Minecraft.getInstance();
+        client.execute(() -> {
+            if (client.player != null) {
+                client.player.sendSystemMessage(message);
+            }
+        });
+    }
+}
