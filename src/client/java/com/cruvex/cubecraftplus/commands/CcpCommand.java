34ad5425557a19f8c2eaff0bf76e1397b330944b
cruvex <cruvexx@gmail.com>
2026-09-19 23:42:07 +0200

    chore: only register /ccp probe and /ccp query in a dev environment

--- src/client/java/com/cruvex/cubecraftplus/commands/CcpCommand.java
+++ src/client/java/com/cruvex/cubecraftplus/commands/CcpCommand.java
@@ -4,6 +4,7 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.fabricmc.loader.api.FabricLoader;
 
 /** {@code /ccp} and {@code /cubecraftplus}: the root every subcommand hangs off. */
 public final class CcpCommand {
@@ -14,15 +15,19 @@ public final class CcpCommand {
     }
 
     private static LiteralArgumentBuilder<FabricClientCommandSource> build(String name) {
-        return ClientCommandManager.literal(name)
+        LiteralArgumentBuilder<FabricClientCommandSource> root = ClientCommandManager.literal(name)
                 .executes(ctx -> ConfigCommand.open(ctx.getSource().getClient()))
                 .then(ConfigCommand.node())
-                .then(DebugCommand.node())
-                .then(ChestFinderCommand.node())
-                .then(ProbeCommand.node())
-                .then(QueryCommand.node())
                 .then(FriendsCommand.node())
-                .then(VersionCommand.node());
+                .then(VersionCommand.node())
+                .then(ChestFinderCommand.node())
+                .then(DebugCommand.node());
+
+        if (FabricLoader.getInstance().isDevelopmentEnvironment()) {
+            root.then(ProbeCommand.node())
+                    .then(QueryCommand.node());
+        }
+        return root;
     }
 
     private CcpCommand() {
